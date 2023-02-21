91d6ef133311f94a2daf717ebf00b85b037fc6f2
Lorenzo <lzurzolo@gmail.com>
2023-02-21 17:43:16 -0500

    Added bones between joint positions

---
+++ UnitySample/Assets/Scripts/JointConnector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointConnector : MonoBehaviour
+{
+    public GameObject jointA;
+    public GameObject jointB;
+
+    private LineRenderer _lineRenderer;
+
+    private void Awake()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Start()
+    {
+        ConfigureLine();
+    }
+
+    private void Update()
+    {
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        _lineRenderer.SetPosition(0, jointA.transform.position);
+        _lineRenderer.SetPosition(1, jointB.transform.position);
+    }
+
+    private void ConfigureLine()
+    {
+        _lineRenderer.positionCount = 2;
+        UpdateLine();
+    }
+}
